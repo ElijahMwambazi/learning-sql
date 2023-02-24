50d9700d2b6a1ab6ee2da8564d8a7c5985ecc6bd
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-02-24 12:29:41 +0200

    Done with day 1

---
+++ Day 1/Day-1-challenges.sql
@@ -0,0 +1,26 @@
+-- DAY 1
+-- Challenge SELECT
+-- select first_name, last_name, email from customer
+
+-- Challenge ORDER BY
+-- select * from customer order by first_name desc
+
+-- Challenge SELECT DISTINCT
+-- select distinct price from payment order by price desc
+
+-- End of day Challenges
+-- CHALLENGE 1
+-- select * from address
+-- select distinct district from address
+
+-- CHALLENGE 2
+-- select * from rental
+-- select distinct rental_date from rental order by rental_date desc limit 1
+
+-- CHALLENGE 3
+-- select * from film
+-- select distinct count(title) from film
+
+-- CHALLENGE 4
+-- select * from customer
+-- select distinct count(last_name) from customer
